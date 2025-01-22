cbf24619fe272bb43818290e2872c885dc033465
devmwall <me@mattwall.dev>
2025-01-21 22:06:54 -0500

    Adding actual integration with chatgpt

--- backend/services/receiptService.js
+++ backend/services/receiptService.js
@@ -11,18 +11,35 @@ class ReceiptService {
         this.OcrService = new OcrService();
         this.ChatGptService = new ChatGptService();
     }
+
+    parseContent(content) {
+        // Remove the ```json and ``` markers
+        const cleanedContent = content.replace(/```json\n/, '').replace(/```$/, '');
+      
+        // Parse the JSON string
+        try {
+            const parsed = JSON.parse(cleanedContent);
+            return parsed;
+        } catch (error) {
+            console.error("Failed to parse JSON:", error);
+            return null;
+        }
+      }
+
     async processImage(imageData) {
         // Process the image using OCR service
-        //const ocrResult = await this.OcrService.processImageLocally(imageData.path);
-        //const chatGptResults = //await this.ChatGptService.retrieveReceiptJson(ocrResult.text);
-        //const parsedContent = parseContent(chatGptResults.content)
+        const ocrResult = await this.OcrService.processImageLocally(imageData.path);
+        const chatGptResults = await this.ChatGptService.retrieveReceiptJson(ocrResult.text);
+        const parsedContent = this.parseContent(chatGptResults.content);
         //console.log(parsedContent);
-        return importedObject;
+        return parsedContent;
         /*
           1.Now that we have analyzed content, we need to generate them a random site
           2. This will use mongodb to create a url that corresponds to the list
         */
     }
+
+    
 
     //URL Will be test/nanoid
     async generateUrl(imageReturnObject){
